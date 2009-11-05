22e640e9f2d0bde447651629a7e137e4602b7c76
fede.caceres@gmail.com <fede.caceres@gmail.com@a61bdbf2-bc00-11de-b52b-63f31fbada87>
2009-11-05 00:16:35 +0000

    Primeros experimentos de SQLalchemy, y son prometedores.

---
+++ trunk/src/data/entity.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+class Entity(object):
+    def __init__(self, **kw):
+	'''automatic attribute mapping'''
+	for k, v in kw.iteritems():
+	      setattr(self, k, v)
+    
+    def __repr__(self):
+	'''Indicar para que tabla es entidad y que campos lo identifican'''
+	return "<Entity %s %s>" % (self.table_name, " ".join(["=".join((x, str(getattr(self, x)))) for x in self.relevant_columns]))
+
+def EntityFactory(table=None):
+    '''Crear una nueva clase automágicamente'''
+    e = type.__new__(type, str("Entity "+table.name), (Entity,), {})
+    # almacenar la tabla a la que hace referencia
+    e.table_name = table.name
+    # almacenar la lista de campos que son relevantes para esta entidad, como lo son los PK y campos UNIQUE
+    e.relevant_columns = []
+    for c in table.columns:
+	if c.primary_key or c.unique:
+	    e.relevant_columns.append(c.name)
+    return e
