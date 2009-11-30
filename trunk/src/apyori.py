58083f3bb6fb548d665d1931a6bba46008c1cd8b
fede.caceres@gmail.com <fede.caceres@gmail.com@a61bdbf2-bc00-11de-b52b-63f31fbada87>
2009-11-30 03:15:20 +0000

    Lento pero seguro... Ya nos podemos conectar a las bases de datos, y ya se genera por debajo un log con todas las tablas de la BD.
    Ahora solo hace falta presentar la lista de tablas con sus columnas (treeview) y permitir al usuario seleccionar la tabla y como operar sobre sus datos.

--- trunk/src/apyori.py
+++ trunk/src/apyori.py
@@ -9,13 +9,20 @@ from ui import wizzardpages
 
 import string
 
-app = wx.PySimpleApp()
+app = wx.PySimpleApp(redirect = False)
 wizard = AeroWizard(u"aPyori")
+
 # load pages
 page1 = wizzardpages.Page_DataOrigin(wizard)
 page_dbsel = wizzardpages.Page_DatabaseSelector(wizard)
+page_connecting = wizzardpages.Page_Connecting(wizard)
+page_refine = wizzardpages.Page_Refine(wizard)
+
 # chain pages
 page1.Chain({"database":page_dbsel})
+page_dbsel.Chain({"database":page_connecting})
+page_connecting.Chain({"database":page_refine})
+
 # init and run wizard
 wizard.start_page = page1
 wizard.RunWizzard()
--- trunk/src/data/__init__.py
+++ trunk/src/data/__init__.py
@@ -1,1 +1,49 @@
- 
+# -*- coding: utf-8 -*-
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+
+import entity
+
+
+class SQLDataSource(object):
+    def __init__(self, connection_string, parameters = None):
+        self.connection_string = connection_string
+        self.parameters = parameters
+        
+        self.engine = None
+        self.metadata = None
+        self.session = None
+        self.entities = {}
+       
+        
+    def connect(self):
+        # conectar
+        self.engine = sqlalchemy.create_engine(self.connection_string)
+        self.metadata = sqlalchemy.MetaData(bind=self.engine)
+        self.metadata.reflect()
+
+        # iniciar "sesión"
+        Session = sqlalchemy.orm.sessionmaker(bind=self.engine) # ver como funciona esto para hacer un "entity maker"
+        self.session = Session()
+        
+        # registrar entidades usando el mapeador automático de entidades :D
+        entities = {}
+        for table in self.metadata.sorted_tables:
+            if self.connection_string[0:6] == "sqlite" and table.name[0:6] == "sqlite":
+                # saltar las tablas del sistema
+                continue 
+            # crear entidad nueva
+            self.entities[table.name] = entity.EntityFactory(self.metadata.tables[table.name])
+            # registrar al ORM
+            sqlalchemy.orm.mapper(self.entities[table.name], self.metadata.tables[table.name])
+           
+        
+    def get_tables(self):
+        '''
+        Retorna una lista de tablas
+        '''
+        return self.metadata.sorted_tables
+        
+        
+        
+        
