60bcd92a5bd182f91ffec5afb5167785360598d3
sergio.sta <sergio.sta@a61bdbf2-bc00-11de-b52b-63f31fbada87>
2009-12-05 23:09:55 +0000

    aqui ya esta manejando el minimumReq osea la etapa 1 luego te cuento como fuciona hice en 15 min. pq toda la tarde me tranque en un problema y quise solucinar con recursivas y casi colapso. y hace media horar se me ocurrio que podia hacer con "for" nomas bue cosas que pasan

--- src/kernel/clases.py
+++ src/kernel/clases.py
@@ -19,21 +19,41 @@ class Nucleo:
         '''
         
         
-    def minimumReq(self,variables=[],data=[],min=1):
+    def minimumReq(self,datos={},min=0.70):
         '''variables son la lista de variables = ['x1','x2','x3'....]
            data es toda la lista de la BD 
            min el el minimo de requerimiento 0...1 '''
         res=[]
-        for i in range(1,len(varibles)):
+        variables=[]
+        popular=[]
+        
+        for i in datos:
+          for j in set(datos[i]):  
+                variables.append(j)
+          
+                      
+        for i in range(1,len(variables)):
             #generar las todas las combinaciones de varibldes de 1 a n logitud
-            res.append(xuniqueCombinations(varibales,i))
+            res.append(xuniqueCombinations(variables,i))
             
-            
-            for i in res:
-                for j in i:
-                    #si pasa la prueba de minReq
-                    self.canditatos.append(j)
-                    pass
+        for i in datos:
+            for j in i:
+                popular.append([])
+            break
+        for i in datos:
+            for x,y in zip(datos[i],popular):
+                y.append(x)
+        print popular
+        for i in res:
+           for j in i:
+              #si pasa la prueba de minReq
+              contador = 0
+              for ind in popular:
+                  if j in ind:
+                     contador += 1
+              if (contador > (len(popular)*min)):
+                  self.candidatos.append(j)
+              pass
 
 
     def generarReglas(self):
@@ -41,10 +61,9 @@ class Nucleo:
             #generar las todas las premutacios de caditatos de 2 logitud
         res = xcombinations(self.candidatos,2)
         for i in res:
-            izq=set(i[0])
-            der=set(i[1])
+            
     
-            if len(der&izq) == 0: #interseccion de izq y der
+            if len(set(i[0])&set(i[1])) == 0: #interseccion de izq y der
                 nueva_regla = Regla
                 nueva_regla.izq=i[0]
                 nueva_regla.der=i[1]
