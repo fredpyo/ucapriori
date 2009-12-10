86f9b1d29db3f0641e4b0da4de790c808df75749
fede.caceres@gmail.com <fede.caceres@gmail.com@a61bdbf2-bc00-11de-b52b-63f31fbada87>
2009-12-10 23:42:28 +0000

    Mejoras a la ventana final. - Ahora el log y el grafo aparecen en las pestañas de un tabbed window (o un notebook como se lo conoce en wx). - Se guarda por el momento en la misma ubicación que el apyori.py un grafico "graph.png"... - La generación de los gráficos se realiza en ui.graphig.Graphing, para no contaminar tanto a ui.wizzardpages - Las reglas finales se implemen como {x, y} --> {a, b} en vez del viejo y horrible [u'x', u'y'] --> [u'a', u'b']... much better

---
+++ trunk/src/ui/graphing.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Dec 10, 2009
+
+@author: fede
+La idea de esto es manejar la graficación por separado... manejo de archivos temporales, etc.
+'''
+import pydot
+import tempfile
+import wx
+
+class Graphing(object):
+    def __init__(self):
+        self.graph = pydot.Dot('rt', graph_type='digraph')
+        self.dir = tempfile.mkdtemp() 
+    
+    def graph_rules(self, rules):
+        '''
+        Grafica las reglas... usamos la misma clase que en kernel.clases
+        '''
+        for i in rules:
+            node_izq = pydot.Node(i.como_cadena("izq", "\\n"), style="filled", fillcolor="#a7ff9f", color="#12df00", fontsize="10.0")
+            node_der = pydot.Node(i.como_cadena("der", "\\n"), style="filled", fillcolor="#a7ff9f", color="#12df00", fontsize="10.0")
+            self.graph.add_node(node_izq)
+            self.graph.add_node(node_der)
+            edge = pydot.Edge(node_izq, node_der, color="#669966", labelfontcolor="#669966", fontsize="8.0", label="Confianza=%d%%\\nSensibilidad=%d%%" % (i.confianza*100,i.sensibilidad*100))
+            #edge = pydot.Edge(node_izq, node_der, color="#669966", labelfontcolor="#669966", fontsize="11.0", label="asdads")
+            self.graph.add_edge(edge)
+        self.graph.write_png(self.dir+'graph.png')
+        
+    def get_wx_image(self):
+        '''
+        Retorna una xw image
+        '''
+        return wx.Image(self.dir+'graph.png', wx.BITMAP_TYPE_ANY)
+    
+    def save(self, filename):
+        return self.graph.write_png(filename) 
